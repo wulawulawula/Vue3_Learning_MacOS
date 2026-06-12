3e7936138744a436fa569f369a162a583cc4b343
oulaoulaoula <m17376757163@163.com>
2026-06-12 11:25:46 +0800

    commit -m -s 'hooks的使用'

---
+++ 备份/19_hooks的使用/App.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <Person ref="person" :list="personList" />
+</template>
+
+
+<script lang="ts" setup>
+import Person from './components/Person.vue'
+import { type personType } from '@/types'
+import { ref } from 'vue';
+defineOptions({
+    name: 'App'
+});
+
+let personList:personType = [
+    {
+        name: '张三',
+        age: 18,
+        id: 'iauhfiahufiah01'
+    },
+    {
+        name: '张三',
+        age: 19,
+        id: 'iauhfiahufiah02'
+    },
+    {
+        name: '张三',
+        age: 20,
+        id: 'iauhfiahufiah03'
+    }
+]
+</script>
+
+<style scoped></style>
