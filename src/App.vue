5a2963d46d9e66f8ea58d5aaaab7fe93a1461551
oulaoulaoula <m17376757163@163.com>
2026-06-17 16:06:02 +0800

    commit -m -s  ‘修改引入’

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts" setup>
 import Count from './components/Count.vue';
-import Talk from './components/talk.vue';
+import Talk from './components/Talk.vue';
 defineOptions({
     name: 'App',
 })
